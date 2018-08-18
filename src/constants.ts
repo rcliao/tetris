1d97cbc1faf58706f43ef59f1fddec80696daf20
Eric Liao <rcliao01@gmail.com>
2018-08-18 10:18:30 -0700

    refactor: add constants implementation

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const actionKeyMap: { [key: number]: string; } = {
+    32: "drop",
+    37: "left",
+    38: "rotate",
+    39: "right",
+    40: "down",
+    72: "left",
+    74: "down",
+    75: "rotate",
+    76: "right",
+};
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
 import { Board } from "./board";
+import { actionKeyMap } from "./constants";
 
-const actionKeyMap: { [key: number]: string; } = {
-    32: "drop",
-    37: "left",
-    38: "rotate",
-    39: "right",
-    40: "down",
-    72: "left",
-    74: "down",
-    75: "rotate",
-    76: "right",
-};
 
 const canvas = <HTMLCanvasElement> document.querySelector("#main");
 const ctx = canvas.getContext("2d");
